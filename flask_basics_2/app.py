1143617cd956d6fe8694ef959294eaf772694b28
Q1ngEn <158569639+Q1ngEn@users.noreply.github.com>
2024-07-18 08:58:50 +0800

    Add files via upload

---
+++ flask_basics_2/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/chickens')
+def chickens():
+    return render_template('chickens.html')
+
+@app.route('/pandas')
+def pandas():
+    return render_template('pandas.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
